19d3b2f65a0a62f6f567350531e9381b67ca517d
dixithlara <110405846+dixithlara@users.noreply.github.com>
2022-11-18 19:39:16 +0530

    Update __init__.py

--- myprojectify/website/__init__.py
+++ myprojectify/website/__init__.py
@@ -1,18 +1,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
+from flask_uuid import FlaskUUID
 from flask_login import LoginManager
 
-db = SQLAlchemy()
-DB_NAME = "database.db"
-def create_app():
-    app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'ABCDEFGH IJKLMNI'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
-    db.init_app(app)
+db=SQLAlchemy()
+DB_name='database.db'
 
-    login_manager  = LoginManager()
-    login_manager.login_view = 'auth.login'
+def create_app():
+    app=Flask(__name__)
+    FlaskUUID(app)
+    app.config['SECRET_KEY']='asdfghjkl'
+    app.config['SQLALCHEMY_DATABASE_URI']=f'sqlite:///{DB_name}'
+    db.init_app(app)
     
     from .views import views
     from .auth import auth
@@ -20,12 +20,12 @@ def create_app():
     app.register_blueprint(views,url_prefix='/')
     app.register_blueprint(auth,url_prefix='/')
 
+    from .models import User,Note
     
-
     create_database(app)
     
-    login_manager=LoginManager()
-    login_manager.login_view='view.home'
+    login_manager=LoginManager(app)
+    login_manager.login_view='auth.login'
     login_manager.init_app(app)
      
     @login_manager.user_loader
@@ -35,8 +35,10 @@ def create_app():
     return app
 
 def create_database(app):
-    if not path.exists('web1/'+DB_NAME):
+    if not path.exists('web1/'+DB_name):
        with app.app_context(): 
         db.create_all( )
         print('created database!')
        
+
+
