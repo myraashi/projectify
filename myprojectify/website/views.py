8532efe54eeb397ec6e39a5c3a66dd458f224242
dixithlara <110405846+dixithlara@users.noreply.github.com>
2022-11-18 19:40:37 +0530

    Update views.py

--- myprojectify/website/views.py
+++ myprojectify/website/views.py
@@ -1,16 +1,35 @@
-from flask import Blueprint, render_template
-from flask_login import login_user, login_required, logout_user
+#from crypt import methods
+from flask import Blueprint,render_template,request,flash,json,jsonify
+from flask_login import login_required,current_user
+from web1 import db
+from .models import Note
+views=Blueprint('views',__name__)
 
-views = Blueprint('views',__name__)
-
-@views.route('/')
+@views.route('/',methods=['GET','POST'])
+#@login_required
 def home():
-    return render_template("index.html")
+    if request.method=='POST':
+        note=request.form.get('Note')
 
-@views.route('/')
-def signup():
-    return render_template("signup.html")
+        if len(note) < 1:
+            flash('Note too short!',category='error')
+        else:
+            new_note=Note(data=note,user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+            flash('Note successfully added!',category='success')
 
-@views.route('/')
-def features():
-    return render_template("features.html")
+    return render_template("index.html",user=current_user)
+
+# @views.route('/delete-note',methods=['POST'])
+# def delete_note():
+#     note=json.loads(request.data)
+#     noteId=note['noteId']
+#     note=Note.query.get(noteId)
+
+    # if note:
+    #     if Note.user_id == current_user.id:
+    #        db.session.delete(note)
+    #        db.session.commit()
+           
+    #     return jsonify({})
